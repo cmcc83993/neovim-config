42cc49d39871f40af5f3f09d33a86b895f16ce4f
Conor McCormick <16789756+cmcc83993@users.noreply.github.com>
2024-01-08 16:23:09 -0500

    Reorder init.lua includes.

--- lua/conor/init.lua
+++ lua/conor/init.lua
@@ -1,7 +1,7 @@
 
 -- Note: Remap needs to be included before Lazy since leader
 -- needs to be setup before Lazy is setup
+require("conor.set")
 require("conor.remap")
 require("conor.lazy")
-require("conor.set")
 require("conor.python")
